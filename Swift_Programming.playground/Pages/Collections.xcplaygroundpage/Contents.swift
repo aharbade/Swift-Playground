8de6bd3fbdd0f75f9ab2c67e27c525a6cf91fc64
Anup Harbade <anupharbade.ah@gmail.com>
2019-02-20 08:55:14 -0600

    Adding Strings and Collections pages in the Playground
    Adding Strings and Collections pages in the Playground

---
+++ Swift_Programming.playground/Pages/Collections.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//Swift has 3 types of collections.
+//Array
+//Dictionary
+//Set
+
+
+//constant array
+let directions = ["east", "west", "north", "south"]
+
+//access array element using its index
+directions[1] //west
+
+//variable array
+var cousins = ["aman", "mahi", "saurabh", "sonal"]
+cousins.append("nikita")
+
+//another way of creating an array
+var countries = [String]()
+countries.append("India")
+countries.append("USA")
+countries.append("Australia")
+
+
+let states : [String] = Array<String>()
+
+//: [Next](@next)
---
+++ Swift_Programming.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//This is a sample string
+let str = "Hello, playground"
+
+//Combined String
+let str1 = "This is a"
+let str2 = "combined string"
+
+
+let combinedStr = str1 + " " + str2
+
+//Multi-line string literal
+let multilineStr = """
+This is a
+multi-line 
+string.
+"""
+
+//String interpolation
+let num1 = 3
+let num2 = 2
+
+let interpolatedString = "\(num1) + \(num2) = \(num1+num2)"
+
+//: [Next](@next)
